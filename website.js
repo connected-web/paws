43e3926018a419b6119318773e09bd92a223b951
John Beech <john.beech@bbc.co.uk>
2016-08-10 10:54:56 +0100

    Host screenshots via website server

--- website.js
+++ website.js
@@ -1,11 +1,16 @@
+const express = require('express');
+const monitor = require('product-monitor');
+
 /*
  Run node website.js, or npm run website to start this script.
  */
-var monitor = require('product-monitor');
-var server = monitor({
+monitor({
   "serverPort": 8080,
   "productInformation": {
     "title": "PAWS Viewer",
   },
   "userContentPath": "website"
+}, (instance) => {
+  instance.server.use('/screenshots', express.static(__dirname + '/screenshots'));
+  instance.listen();
 });
