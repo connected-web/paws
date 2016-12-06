e2495a0888bdab1f420f5166a87578e61bc0f49b
John Beech <john.beech@bbc.co.uk>
2016-12-06 19:30:26 +0000

    Rewrite of PAWS to run based off a schedule instead of self-exploration

---
+++ paws-start.js
@@ -0,0 +1,35 @@
+const find = require('promise-path').find;
+const today = require('./lib/today')
+const paws = require('./paws');
+
+Promise.accept(console.log('Start PAWS'))
+  .then(() => {
+    return find('products/*.json');
+  })
+  .then((paths) => {
+    return paths.map((path) => {
+      return require(`./${path}`);
+    });
+  })
+  .then((configs) => {
+    return configs.map((config) => {
+      return paws.monitor(config);
+    });
+  })
+  .catch((ex) => {
+    console.error('Caught exception', ex, ex.stack);
+  });
+
+const currentDay = today();
+
+function checkDay() {
+  if (currentDay !== today()) {
+    console.log('Day has changed, exiting service to force restart');
+    process.exit(0);
+  } else {
+    const fiveMinutesInMs = 5 * 60 * 1000;
+    setTimeout(checkDay, fiveMinutesInMs);
+  }
+}
+
+checkDay();
